8afa968b78ec01b736528466a7094004df033aae
Unknown <harisunnyt@gmail.com>
2020-01-04 10:06:41 +1030

    Capy has a better posture when standing

---
+++ Assets/_Game/Scripts/Editor/PickupableItemEditor.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(PickupableItem), true)]
+public class PickupableItemEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        PickupableItem pickupableItem = (PickupableItem)target;
+
+        if (GUILayout.Button("Set Transform"))
+        {
+            pickupableItem.PickupableItemData.Position = pickupableItem.transform.localPosition;
+            pickupableItem.PickupableItemData.EulerRotation = pickupableItem.transform.localRotation.eulerAngles;
+        }
+    }
+}
