9b5e89ee188e03617c1502a622e0093735367a3b
Tyler Roach <tylerroach3@gmail.com>
2020-01-12 03:09:41 +1030

    Just played around with a main menu to look at something different for a bit

---
+++ Assets/_Game/Scripts/UI/ParallaxScrolling.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxScrolling : MonoBehaviour
+{
+    [SerializeField]
+    private float scrollSpeed;
+
+    [SerializeField]
+    private float loopValue;
+
+    private Vector3 startPos;
+
+    void Start()
+    {
+        startPos = transform.position;
+    }
+
+    void Update()
+    {
+        float newPos = Mathf.Repeat(scrollSpeed * Time.time, loopValue);
+        transform.localPosition = startPos - Vector3.left * newPos;
+    }
+}
