67bdb4c00855dd61493c8842cfcded87d10a37cd
Oscar <chiyuiri@gmail.com>
2020-01-13 23:07:42 +1030

    Scene transition effect

---
+++ Assets/_Game/Scripts/UI/LevelLoader.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    public Animator transition;
+
+    public int levelIndex;
+
+    public float transitionTime = 1f;
+
+    public void LoadLevel()
+    {
+        StartCoroutine(LoadLevel(levelIndex));
+    }
+
+    IEnumerator LoadLevel(int levelIndex)
+    {
+        transition.SetTrigger("Start");
+
+        yield return new WaitForSeconds(transitionTime);
+
+        SceneManager.LoadScene(levelIndex);
+    }
+}
