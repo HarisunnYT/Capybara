a67525c28267c5517a3943ec0a909e23e5ab692c
Unknown <harisunnyt@gmail.com>
2020-02-25 19:52:01 +1030

    Glass shatters

---
+++ Assets/_Game/Scripts/Objects/FractureWithForce.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FractureWithForce : MonoBehaviour
+{
+    [SerializeField]
+    private float force;
+
+    private FracturedObject fracturedObject;
+
+    private void Start()
+    {
+        fracturedObject = GetComponentInParent<FracturedObject>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.relativeVelocity.magnitude > force)
+        {
+            fracturedObject.Explode(collision.contacts[0].point, collision.relativeVelocity.magnitude);
+            gameObject.SetActive(false);
+        }
+    }
+}
