583c7e08ddd4beb150a3ec81f542d97df9a448ca
Unknown <harisunnyt@gmail.com>
2020-03-03 19:14:07 +1030

    Collecting key takes you back to the main menu

---
+++ Assets/_Game/Scripts/Interactables/Consumable.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Consumable : MonoBehaviour
+{
+    protected virtual void OnPickedUp() { }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.GetComponentInParent<CapyMovementController>())
+        {
+            OnPickedUp();
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.GetComponentInParent<CapyMovementController>())
+        {
+            OnPickedUp();
+        }
+    }
+}
